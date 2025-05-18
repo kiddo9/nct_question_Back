b20a9acaafa1b957aa62a2e788dadda5c07b2b1f
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-05-18 10:33:29 +0100

    update users table

---
+++ client/src/components/UsersTable/UserPagination.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const UserPagination = ( { getUsers, sortedUsers, currentPage, setCurrentPage, numberPerPage, setNumberPerPage }) => {
+  return (
+    <div className="flex items-center justify-between">
+          <div className="text-sm text-gray-700">
+            <span className="font-medium">{getUsers.length}</span> results
+          </div>
+          <div className="flex space-x-2">
+            <select className='bg-white border border-gray-300 text-gray-500 hover:bg-gray-50 px-2 py-1 lg:px-4 lg:py-2 text-xs lg:text-sm font-medium rounded-md' value={numberPerPage} onChange={(e) => {
+                setNumberPerPage(parseInt(e.target.value))
+                setCurrentPage(1)
+            }} name="numberPerPage" id="">
+              <option value="6">6</option>
+              <option value="10">10</option>
+            </select>
+            {sortedUsers.length > numberPerPage && (
+            <>
+              <button disabled={currentPage <= 1} onClick={() => setCurrentPage(currentPage - 1)} className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-50 px-2 py-1 lg:px-4 lg:py-2 text-xs lg:text-sm font-medium rounded-md">
+                Previous
+              </button>
+              {Array.from({ length: Math.ceil(getUsers.length / numberPerPage) }, (_, index) => (
+                <button
+                  key={index}
+                  className={`px-2 py-1 lg:px-4 lg:py-2 text-xs lg:text-sm font-medium rounded-md ${
+                    currentPage === index + 1 ? "bg-[#6674BB] text-white" : "bg-white border border-gray-300 text-gray-500 hover:bg-gray-50"
+                  }`}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              ))}
+              <button disabled={currentPage >= Math.ceil(getUsers.length / numberPerPage)} onClick={() => setCurrentPage(currentPage + 1)} className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-50 px-2 py-1 lg:px-4 lg:py-2 text-xs lg:text-sm font-medium rounded-md">
+                Next
+              </button>
+            </>
+            )}
+          </div>
+        </div>
+  )
+}
+
+export default UserPagination
---
+++ client/src/components/UsersTable/UserRow.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { Edit, Trash2, Eye} from 'lucide-react';
+
+const UserRow = ({ user, selectedRows, toggleSelectRow }) => {
+    const StatusBadge = ({ status }) => {
+        let bgColor = '';
+        let statusText = '';
+        
+        switch(status) {
+          case 1:
+            bgColor = 'bg-green-100 text-green-800';
+            statusText = 'Active';
+            break;
+          case 0:
+            bgColor = 'bg-yellow-100 text-yellow-800';
+            statusText = 'Inactive';
+            break;
+          case -1:
+            bgColor = 'bg-gray-100 text-gray-800';
+            statusText = 'Deleted';
+            break;
+          case 'active':
+            bgColor = 'bg-blue-100 text-green-800';
+            statusText = 'Active';
+            break;
+          case 'inactive':
+            bgColor = 'bg-yellow-100 text-yellow-800';
+            statusText = 'Inactive';
+            break;
+          case 'deleted':
+            bgColor = 'bg-gray-100 text-gray-800';
+            statusText = 'Deleted';
+            break;
+          default:
+            bgColor = 'bg-blue-100 text-blue-800';
+            statusText = 'Unknown';
+        }
+        
+        return (
+          <span className={`px-2 py-1 text-xs font-medium rounded-full ${bgColor}`}>
+            {statusText}
+          </span>
+        );
+      };
+
+      const TrueFalseBadge = ({ status }) => {
+        let bgColor = '';
+        let statusText = '';
+        
+        if(status) {
+          bgColor = 'bg-green-100 text-green-800';
+          statusText = 'Yes';
+        } else {
+          bgColor = 'bg-red-100 text-red-800';
+          statusText = 'No';
+        }
+        
+        return (
+          <span className={`px-2 py-1 text-xs font-medium rounded-full ${bgColor}`}>
+            {statusText}
+          </span>
+        );
+      };
+
+  return (
+    <tr 
+    key={user.id} 
+    className={`${selectedRows.includes(user.id) ? 'bg-blue-50' : 'hover:bg-gray-50'}`}
+    >
+    <td className="p-4">
+      <input
+        type="checkbox"
+        className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+        checked={selectedRows.includes(user.id)}
+        onChange={() => toggleSelectRow(user.id)}
+      />
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+      {user.id}
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
+      {user.name}
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
+      {user.role}
+    </td>
+    <td className="px-4 py-4 text-sm text-gray-700 max-w-xs truncate">
+      {user.email}
+      
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
+      <TrueFalseBadge status={user.loggedIn} />
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
+      <TrueFalseBadge status={user.passwordSet} />
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap">
+      <StatusBadge status={user.status} />
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-700">
+      {new Date(user.createdAt).toUTCString()}
+    </td>
+    <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
+      <div className="flex justify-end space-x-2">
+        <button className="text-blue-600 hover:text-blue-900">
+          <Eye size={18} />
+        </button>
+        <button className="text-green-600 hover:text-green-900">
+          <Edit size={18} />
+        </button>
+        <button className="text-red-600 hover:text-red-900">
+          <Trash2 size={18} />
+        </button>
+      </div>
+    </td>
+  </tr>
+  )
+}
+
+export default UserRow
