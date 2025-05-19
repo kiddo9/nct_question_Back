eb4123fc669f3bc4592f9e2b2a7d3954cdaa986b
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-05-19 18:00:48 +0100

    preview and groups pages

---
+++ client/src/pages/Groups.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import Loader from '../components/Loader'
+import AddButton from '../components/AddButton'
+import { Link } from 'react-router-dom'
+import { useQuestionGroupHook } from '../hooks/questionGroupHook'
+import { CircleX } from 'lucide-react'
+
+const Groups = () => {
+    const { questionGroups, loader: groupLoader } = useQuestionGroupHook()
+    console.log(questionGroups)
+
+    const StatusBadge = ({ status }) => {
+        let bgColor = '';
+        let statusText = '';
+        
+        switch(status) {
+          case 1:
+            bgColor = 'bg-green-100 text-green-800';
+            statusText = 'Active';
+            break;
+          case 0:
+            bgColor = 'bg-yellow-100 text-yellow-800';
+            statusText = 'Inactive';
+            break;
+          case -1:
+            bgColor = 'bg-gray-100 text-gray-800';
+            statusText = 'Deleted';
+            break;
+          default:
+            bgColor = 'bg-blue-100 text-blue-800';
+            statusText = 'Unknown';
+        }
+        
+        return (
+          <span className={`px-2 py-1 text-xs font-medium rounded-full ${bgColor}`}>
+            {statusText}
+          </span>
+        );
+      };
+  return (
+    <div className="rounded-lg lg:px-2 py-8">
+        {groupLoader && <Loader />}
+        <ToastContainer />
+        <div className="flex flex-col  bg-white rounded-2xl mx-auto shadow py-2 w-[97vw] lg:w-[calc(100vw-270px)] ">
+            <div className="flex justify-end  gap-2 items-center px-5 py-3 shadow-md">
+                <div className='cursor-pointer flex justify-center rounded-md bg-[#6674BB] px-2 py-2 text-white'>
+                    <img src="/edit.svg" alt="edit" width={25} height={25} />
+                </div>
+                
+                <Link to={'/admin/user/groups/create'}>
+                    <AddButton>Add Group</AddButton>
+                </Link>
+            </div>
+            <div className=' overflow-y-scroll max-h-[calc(100vh-232px)]'>
+            {
+                questionGroups.map((group) => (
+                    <div className='py-4 border-t-2 border-gray-300 flex justify-between items-center px-5'>
+                        <h1 className='md:text-lg text-[16px]  text-black'>{group.title}</h1>
+                        <div className='flex gap-10 items-center'>
+                            <StatusBadge status={group.active_status} />
+                            <CircleX className='cursor-pointer stroke-[#989898] hover:stroke-[#6674BB]' />
+                        </div>
+                                            
+                    </div>
+                ))
+              
+            }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+const AddGroupModal = () => {
+  return (
+    <div>AddGroupModal</div>
+  )
+}
+
+export default Groups
