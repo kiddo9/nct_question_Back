37a526be2f83456730733df701961b6ed8f96c92
Elechi Paschal <dkido913@gmail.com>
2025-05-27 17:16:11 +0100

    Update Api.jsx

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api = axios.create({
   baseURL:
     import.meta.env.VITE_BACKEND_ENDPOINT ||
-    "http://cbtquestionbank.publicvm.com/api/v3/u/",
+    "http://ec2-44-208-163-45.compute-1.amazonaws.com:3000/api/v3/u/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
