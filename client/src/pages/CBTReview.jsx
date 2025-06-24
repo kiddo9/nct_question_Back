c29cc6eb0351e0757c9c68b86c61805859eb57f2
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-06-24 21:33:55 +0100

    cbt question review functionality all set

--- client/src/pages/CBTReview.jsx
+++ client/src/pages/CBTReview.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ToastContainer } from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify'
 import Fetching from '../components/Fetching'
 import QuestionSelector from '../components/QuestionSelector'
 import QuestionForReview from '../components/QuestionForReview'
@@ -8,6 +8,8 @@ import useQuestionHook from '../hooks/questionHook'
 import useClassHook from '../hooks/classHook'
 import useSectionHook from '../hooks/sectionHook'
 import { useQuestionGroupHook } from '../hooks/questionGroupHook'
+import { useNavigate } from 'react-router-dom'
+import CreateHeader from '../components/CreateHeader'
 
 const CBTReview = () => {
     const [loading, setLoading] = useState(false)
@@ -17,17 +19,30 @@ const CBTReview = () => {
     const { classes, loader: classLoader } = useClassHook()
     const { sections, loader: sectionLoader } = useSectionHook()
     const {opt, loader: optLoader} = useOpt()
+    const nav = useNavigate()
 
-    const transformedQuestions = getQuestion.map((question, index) => ({
+    const cbtQR = JSON.parse(localStorage.getItem('cbt-qr'))
+
+    
+    {/* Check if cbt qr data from local storage is null and handle redirect */}
+    useEffect(() => {
+      if(cbtQR === null) {
+        toast.error('No CBT QR Data found. Redirecting...')
+        setTimeout(() => nav('/admin/user/cbt-qr'), 3000)
+      }
+    }, [nav, cbtQR])
+
+    const transformedQuestions = getQuestion.map((question) => ({
       ...question,
-      number: index + 1,
       options: opt?.filter((option) => option?.question_bank_id == question?.id),
       group: questionGroups.find(group => group.id === question.q_group_id)?.title,
       class: classes.find(cls => cls.id === question.class_id)?.class_name,
       section: sections.find(section => section.id == question.section_id)?.section_name,
     }))
 
-    const filteredQuestions = transformedQuestions
+    const filteredQuestions = transformedQuestions.filter((question) => 
+      cbtQR && question.group === cbtQR.group && question.class === cbtQR.className && question.section === cbtQR.section
+    ).map((question, index) => ({...question, number: index+1}));
 
     const [questions, setQuestions] = useState([]);
 
@@ -74,13 +89,17 @@ const CBTReview = () => {
   return (
     <div className="rounded-lg lg:px-2 py-8">
         <ToastContainer />
-        <div className="flex flex-col  bg-white rounded-2xl mx-auto shadow py-2 w-[100vw]  lg:w-[calc(100vw-270px)] h-[calc(100vh-160px)] ">
+        <div className="flex flex-col  bg-white rounded-2xl mx-auto shadow py-2 w-[100vw] lg:w-[calc(100vw-270px)] h-[calc(100vh-160px)] ">
+            <CreateHeader>
+              <p className='text-lg text-center mx-auto lg:text-2xl font-bold'>{cbtQR.group} - {cbtQR.className} - {cbtQR.section}</p>
+            </CreateHeader>
             {
                 loading || optLoader || questionLoader || groupLoader || classLoader || sectionLoader
                 ? <Fetching /> 
                 
-                : <div className=' py-2 flex flex-col gap-10'>
-                    <div className='flex px-4 flex-wrap gap-2 items-center justify-between w-full'>
+                : <div className=' py-2 mt-5 flex flex-col gap-5 overflow-y-scroll '>
+    
+                    <div className='flex px-4 flex-wrap gap-2 items-center justify-between w-full '>
                       <span>
                         Questions: <span className='font-semibold'>{questions.length}</span>
                       </span>
@@ -91,16 +110,13 @@ const CBTReview = () => {
                         Flagged: <span className='font-semibold text-red-500'>{questions.filter((question) => question.active_status === 0).length}</span>
                       </span>
                       <span>
-                        Filter By:
-                      </span>
-                      <span>
                         Marks: <span className='font-semibold text-[#6674BB]'>{questions.reduce((total, question) => total + question.marks, 0)}</span>
                       </span>
                     </div>
 
-                    <div className='flex flex-col-reverse md:flex-row gap-10 overflow-y-scroll max-h-[calc(100vh-270px)]'>
+                    <div className='flex flex-col md:flex-row gap-10'>
                       <QuestionSelector questions={questions} activeNumber={activeNumber} setActiveNumber={setActiveNumber}/>
-                      <QuestionForReview question={questions.find((question) => question.number === activeNumber)} setActiveNumber={setActiveNumber} questionUpdate={handleQuestionUpdate}/>
+                      <QuestionForReview question={questions.find((question) => question.number === activeNumber)} setActiveNumber={setActiveNumber} questionUpdate={handleQuestionUpdate} totalQuestions={questions.length}/>
                     </div>
                   </div>
             }
