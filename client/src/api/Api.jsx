61379c36b15f67d7286e4414197027ab02444837
Olumide Ayoola <144691310+Olumightty@users.noreply.github.com>
2025-09-13 11:37:20 +0100

    Update Api.jsx

--- client/src/api/Api.jsx
+++ client/src/api/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api = axios.create({
   baseURL:
     import.meta.env.VITE_BACKEND_ENDPOINT ||
-    "https://cbtquestionbank.publicvm.com/api/v3/u",
+    "https://qbank.nct.publicvm.com/api/v3/u",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
